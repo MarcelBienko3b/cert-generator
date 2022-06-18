3b6502069a6d60f1632ead3362fde0ce6d04b57a
MarcelBienko3b <m.bienko.3b19@gmail.com>
2022-06-18 20:50:53 +0200

    UI changes

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Certificate Generator</title>
 </head>
 <body>
@@ -27,9 +30,9 @@
                 echo '<form action="./scripts/set-flag.php" method="post">
                         <table>
                             <tr>
-                                <td>Data</td>
-                                <td>To generate</td>
-                                <td>Remove from database</td>
+                                <td style="font-weight: bold">Data</td>
+                                <td style="font-weight: bold">To generate</td>
+                                <td style="font-weight: bold">Remove from database</td>
                             </tr>';
                 while ($rows = $result->fetch_all()) {
                     foreach ($rows as $row) {
@@ -56,16 +59,16 @@
 
     </div>
 
-    <div class="table-container">
+    <div class="user-container">
         <h2>Add user</h2>
 
             <form action="./scripts/send-user.php" method="post">
 
                 <label for="fname">First name</label><br>
-                <input type="text" name="fname" default=""><br>
+                <input type="text" name="fname" default=""><br><br>
 
                 <label for="lname">Last name</label><br>
-                <input type="text" name="lname" default=""><br>
+                <input type="text" name="lname" default=""><br><br>
 
                 <label for="sign">Sign</label><br>
                 <input type="text" name="sign" required><br><br>
@@ -73,32 +76,34 @@
                 <input type="submit" value="Submit">
 
             </form>
-
     </div>
 
+    <div class="image-container">
+        <h2>Your certificate background:</h2>
+        <img src="cert-background.jpg" alt="Certificate background image">
+    </div>
+
+
     <div class="generator-container">
+        <h2>Generator</h2>
         <form action="./scripts/cert-generate.php" method="post">
+    
+            <label for="font-size">Font size</label><br>
+            <input type="number" name="font-size" require><br><br>
 
-            <div class="image-container">
-                <p>Your certificate background:</p>
-                <img src="cert-background.jpg" alt="Certificate background image">
-            </div>
+            <label for="font-angle">Font angle</label><br>
+            <input type="number" name="font-angle" required><br><br>
 
-            <label for="font-size">Font size</label>
-            <input type="number" name="font-size" require><br>
+            <label for="text-x">X coordinate</label><br>
+            <input type="number" name="text-x" required><br><br>
+            <label for="text-y">Y coordinate</label><br>
+            <input type="number" name="text-y" required><br><br>
 
-            <label for="font-angle">Font angle</label>
-            <input type="number" name="font-angle" required><br>
-
-            <label for="text-x">X coordinate</label>
-            <input type="number" name="text-x" required><br>
-            <label for="text-y">Y coordinate</label>
-            <input type="number" name="text-y" required><br>
-
-            <label for="color">Text color</label>
-            <input type="color" name="color" default="#000000"><br>
+            <label for="color">Text color</label><br>
+            <input type="color" name="color" default="#000000"><br><br>
 
             <input type="submit" value="Generate">
+
         </form>
     </div>
 
